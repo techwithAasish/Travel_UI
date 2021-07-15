614054b8d6cff8011e7de3efdd45a7458aedb85c
techwithAasish <aasish.mahato101@gmail.com>
2021-07-15 13:20:19 +0545

    Initial commit

---
+++ travel_ui/lib/models/destination_model.dart
@@ -0,0 +1,87 @@
+
+import 'package:travel_ui/models/activity.dart';
+
+class Destination{
+  String imageUrl;
+  String city;
+  String country;
+  String description;
+  List<Activity> activities;
+
+  Destination({
+    required this.imageUrl,
+    required this.city,
+    required this.country,
+    required this.description,
+    required this.activities,
+  });
+}
+
+List<Activity> activites = [
+  Activity(
+    imageUrl: 'assets/stmarksbasilica.jpg',
+    name: 'St. Mark\'s Basilica',
+    type: 'Sightseeing Tour',
+    startTimes: ['9:00 am', '11:00 am'],
+    rating: 5,
+    price: 30,
+  ),
+  Activity(
+    imageUrl: 'assets/gondola.jpg',
+    name: 'Walking Tour and Gonadola Ride',
+    type: 'Sightseeing Tour',
+    startTimes: ['11:00 am', '1:00 am'],
+    rating: 4,
+    price: 210,
+  ),
+  Activity(
+    imageUrl: 'assets/murano.jpg',
+    name: 'Murano and Burano Tour ',
+    type: 'Sightseeing Tour',
+    startTimes: ['12:30 am', '2:00 pm'],
+    rating: 3,
+    price: 125,
+  ),
+];
+
+List<Destination> destinations =[
+  Destination(
+    imageUrl: 'assets/venice.jpg',
+    city: 'Venice',
+    country: 'Italy',
+    description: 'Visit Venice for an amazing and unforgettable adventure.',
+    activities: activites,
+  ),
+
+  Destination(
+    imageUrl: 'assets/paris.jpg',
+    city: 'Paris',
+    country: 'France',
+    description: 'Visit Paris for an amazing and unforgettable adventure.',
+    activities: activites,
+  ),
+
+  Destination(
+    imageUrl: 'assets/newdelhi.jpg',
+    city: 'New Delhi',
+    country: 'India',
+    description: 'Visit New Delhi for an amazing and unforgettable adventure.',
+    activities: activites,
+  ),
+
+  Destination(
+    imageUrl: 'assets/saopaulo.jpg',
+    city: 'Sao Paulo',
+    country: 'Brazil',
+    description: 'Visit Sao Paulo for an amazing and unforgettable adventure.',
+    activities: activites,
+  ),
+
+  Destination(
+    imageUrl: 'assets/newyork.jpg',
+    city: 'New York City',
+    country: 'United States',
+    description: 'Visit New York for an amazing and unforgettable adventure.',
+    activities: activites,
+  ),
+];
